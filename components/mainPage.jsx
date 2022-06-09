90cd603e53f4b47e76c5197a708cbfa8b463d0ae
AnnaBilokon <Agnewka89@gmail.com>
2022-06-09 11:40:32 +0200

    code

---
+++ components/mainPage.jsx
@@ -0,0 +1,29 @@
+import React  from 'react';
+import styles from './styles/style.scss';
+import 'antd/dist/antd.css';
+import {Dropdown, Menu} from 'antd';
+
+export const Main = () => (
+	<div>
+		<header>
+		<p className={styles.header}>READ_BOOKA</p>
+		<nav className={styles.nav}>
+            <p className={styles.navLinks}>Главная</p>
+            <p className={styles.navLinks}>Рецензии на книги</p>
+            <p className={styles.navLinks}>Подборки книг</p>
+            <p className={styles.navLinks}>Новинки месяца</p>
+          </nav>
+		</header>
+		<section>
+			<p>
+				
+			</p>
+		</section>
+		<footer className={styles.footer}>
+		<p className={styles.footer_text1} >Книжный блог</p>
+		<p className={styles.footer_text}> © 2022 read_booka</p>
+		<p className={styles.footer_text}> ВСЕ ПРАВА ЗАЩИЩЕНЫ</p>
+		</footer>
+		
+	</div>
+);
---
+++ src/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import {Main,App} from '../components/mainPage';
+
+ReactDOM.render(
+
+    <Main/>,
+    // <App/>,
+    document.getElementById('app')
+);
